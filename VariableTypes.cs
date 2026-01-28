782d503db65efb4814727942ad549ba8b4217aae
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-01-28 16:25:38 +0000

    done?

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -2,4 +2,11 @@
 public class VariableTypes
 {   
     // Add your code here
+    public string AnimalName = "cat";
+    public char GradeLetter = 'A';
+    public int Year = 2026;
+    public double Price = 19.99;
+    public bool IsIndoorPet = true;
+
+
 }
